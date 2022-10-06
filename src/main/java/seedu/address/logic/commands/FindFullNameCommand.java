e5d546ecb16b0242dc122ad401f27b0d4788a832
CedricChia123 <cedricchia01@gmail.com>
2022-10-06 10:04:14 +0800

    Update findfullname command

--- src/main/java/seedu/address/logic/commands/FindFullNameCommand.java
+++ src/main/java/seedu/address/logic/commands/FindFullNameCommand.java
@@ -2,9 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.FullNamePredicate;
+import seedu.address.model.person.Person;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
@@ -26,11 +30,22 @@ public class FindFullNameCommand extends Command {
     }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredPersonList(predicate);
+        List<Person> lastShownList = model.getFilteredPersonList();
+        int targetIndex = 0;
+        for (int i = 0; i < lastShownList.size(); i++) {
+            Person currentPerson = lastShownList.get(i);
+            if (predicate.test(currentPerson)) {
+                targetIndex = i + 1;
+                break;
+            }
+        }
+        if (targetIndex == 0) {
+            return new CommandResult(String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, 0));
+        }
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_PERSONS_INDEX, targetIndex));
     }
 
     @Override
