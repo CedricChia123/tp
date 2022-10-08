24add530214d52eb6292c8c743332a4f7727a6b3
CedricChia123 <cedricchia01@gmail.com>
2022-10-08 22:38:47 +0800

    Update edituserbyname command

--- src/main/java/seedu/address/logic/commands/EditUserByNameCommand.java
+++ src/main/java/seedu/address/logic/commands/EditUserByNameCommand.java
@@ -63,6 +63,7 @@ public class EditUserByNameCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
         List<Person> lastShownList = model.getFilteredPersonList();
 
         int targetIndex = -1;
